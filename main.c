739678386534993b3acd6d40510e3e7f2dc94750
Kirn Gill II <segin2005@gmail.com>
2025-10-04 14:25:34 -0500

    Add clipboard detection, URL buttons, and color-coded input states

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <string.h>
 
 #define IDC_TEXT_FIELD    1001
 #define IDC_LABEL1        1002
@@ -8,8 +9,39 @@
 #define IDC_BUTTON1       1006
 #define IDC_BUTTON2       1007
 #define IDC_BUTTON3       1008
+#define IDC_DOWNLOAD_BTN  1009
+#define IDC_GETINFO_BTN   1010
 
 #define IDD_MAIN_DIALOG   101
+
+// Global variables for text field colors
+HBRUSH hBrushWhite = NULL;
+HBRUSH hBrushLightGreen = NULL;
+HBRUSH hBrushLightBlue = NULL;
+HBRUSH hCurrentBrush = NULL;
+
+BOOL IsYouTubeURL(const char* url) {
+    return (strstr(url, "https://www.youtube.com/watch") != NULL ||
+            strstr(url, "https://youtu.be/") != NULL ||
+            strstr(url, "https://m.youtube.com/watch") != NULL ||
+            strstr(url, "https://youtube.com/watch") != NULL);
+}
+
+void CheckClipboardForYouTubeURL(HWND hDlg) {
+    if (OpenClipboard(hDlg)) {
+        HANDLE hData = GetClipboardData(CF_TEXT);
+        if (hData != NULL) {
+            char* clipText = (char*)GlobalLock(hData);
+            if (clipText != NULL && IsYouTubeURL(clipText)) {
+                SetDlgItemText(hDlg, IDC_TEXT_FIELD, clipText);
+                hCurrentBrush = hBrushLightGreen;
+                InvalidateRect(GetDlgItem(hDlg, IDC_TEXT_FIELD), NULL, TRUE);
+            }
+            GlobalUnlock(hData);
+        }
+        CloseClipboard();
+    }
+}
 
 void ResizeControls(HWND hDlg) {
     RECT rect;
@@ -18,23 +50,33 @@ void ResizeControls(HWND hDlg) {
     int width = rect.right - rect.left;
     int height = rect.bottom - rect.top;
     
-    // Resize URL text field
-    SetWindowPos(GetDlgItem(hDlg, IDC_TEXT_FIELD), NULL, 45, 8, width - 135, 14, SWP_NOZORDER);
+    // Resize URL text field (leave space for buttons)
+    SetWindowPos(GetDlgItem(hDlg, IDC_TEXT_FIELD), NULL, 45, 10, width - 175, 14, SWP_NOZORDER);
     
-    // Resize listbox (leave 90px for buttons + margin)
-    SetWindowPos(GetDlgItem(hDlg, IDC_LIST), NULL, 10, 70, width - 100, height - 80, SWP_NOZORDER);
+    // Position URL buttons
+    int buttonX = width - 125;
+    SetWindowPos(GetDlgItem(hDlg, IDC_DOWNLOAD_BTN), NULL, buttonX, 8, 60, 18, SWP_NOZORDER);
+    SetWindowPos(GetDlgItem(hDlg, IDC_GETINFO_BTN), NULL, buttonX, 28, 60, 18, SWP_NOZORDER);
     
-    // Position buttons on the right
-    int buttonX = width - 80;
-    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON1), NULL, buttonX, 70, 70, 25, SWP_NOZORDER);
-    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON2), NULL, buttonX, 100, 70, 25, SWP_NOZORDER);
-    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON3), NULL, buttonX, 130, 70, 25, SWP_NOZORDER);
+    // Resize listbox (leave space for side buttons)
+    SetWindowPos(GetDlgItem(hDlg, IDC_LIST), NULL, 10, 95, width - 100, height - 105, SWP_NOZORDER);
+    
+    // Position side buttons
+    int sideButtonX = width - 80;
+    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON2), NULL, sideButtonX, 95, 70, 25, SWP_NOZORDER);
+    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON3), NULL, sideButtonX, 125, 70, 25, SWP_NOZORDER);
 }
 
 // Dialog procedure function
 INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
         case WM_INITDIALOG:
+            // Create brushes for text field colors
+            hBrushWhite = CreateSolidBrush(RGB(255, 255, 255));
+            hBrushLightGreen = CreateSolidBrush(RGB(220, 255, 220));
+            hBrushLightBlue = CreateSolidBrush(RGB(220, 220, 255));
+            hCurrentBrush = hBrushWhite;
+            
             // Initialize dialog controls
             SetDlgItemText(hDlg, IDC_LABEL2, "Status: Ready");
             SetDlgItemText(hDlg, IDC_LABEL3, "Items: 0");
@@ -44,8 +86,11 @@ INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPara
             SendDlgItemMessage(hDlg, IDC_LIST, LB_ADDSTRING, 0, (LPARAM)"Sample Video 2.mp4");
             SendDlgItemMessage(hDlg, IDC_LIST, LB_ADDSTRING, 0, (LPARAM)"Sample Video 3.mp4");
             
+            // Check clipboard for YouTube URL
+            CheckClipboardForYouTubeURL(hDlg);
+            
             // Set minimum window size
-            SetWindowPos(hDlg, NULL, 0, 0, 400, 300, SWP_NOMOVE | SWP_NOZORDER);
+            SetWindowPos(hDlg, NULL, 0, 0, 500, 350, SWP_NOMOVE | SWP_NOZORDER);
             
             return TRUE;
             
@@ -55,15 +100,44 @@ INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPara
             
         case WM_GETMINMAXINFO: {
             MINMAXINFO* mmi = (MINMAXINFO*)lParam;
-            mmi->ptMinTrackSize.x = 350;
-            mmi->ptMinTrackSize.y = 250;
+            mmi->ptMinTrackSize.x = 450;
+            mmi->ptMinTrackSize.y = 300;
             return 0;
         }
+        
+        case WM_CTLCOLOREDIT:
+            if ((HWND)lParam == GetDlgItem(hDlg, IDC_TEXT_FIELD)) {
+                HDC hdc = (HDC)wParam;
+                SetBkColor(hdc, GetSysColor(COLOR_WINDOW));
+                return (INT_PTR)hCurrentBrush;
+            }
+            break;
             
         case WM_COMMAND:
             switch (LOWORD(wParam)) {
-                case IDC_BUTTON1:
+                case IDC_TEXT_FIELD:
+                    if (HIWORD(wParam) == EN_CHANGE) {
+                        // Check if it's a paste operation with YouTube URL
+                        char buffer[1024];
+                        GetDlgItemText(hDlg, IDC_TEXT_FIELD, buffer, sizeof(buffer));
+                        
+                        if (IsYouTubeURL(buffer) && hCurrentBrush != hBrushLightGreen) {
+                            // Manual paste of YouTube URL - set to light blue
+                            hCurrentBrush = hBrushLightBlue;
+                        } else if (hCurrentBrush != hBrushLightGreen) {
+                            // Manual input - set to white
+                            hCurrentBrush = hBrushWhite;
+                        }
+                        InvalidateRect(GetDlgItem(hDlg, IDC_TEXT_FIELD), NULL, TRUE);
+                    }
+                    break;
+                    
+                case IDC_DOWNLOAD_BTN:
                     MessageBox(hDlg, "Download functionality not implemented yet", "Download", MB_OK);
+                    break;
+                    
+                case IDC_GETINFO_BTN:
+                    MessageBox(hDlg, "Get Info functionality not implemented yet", "Get Info", MB_OK);
                     break;
                     
                 case IDC_BUTTON2:
@@ -81,6 +155,11 @@ INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lPara
             break;
             
         case WM_CLOSE:
+            // Clean up brushes
+            if (hBrushWhite) DeleteObject(hBrushWhite);
+            if (hBrushLightGreen) DeleteObject(hBrushLightGreen);
+            if (hBrushLightBlue) DeleteObject(hBrushLightBlue);
+            
             EndDialog(hDlg, 0);
             return TRUE;
     }
