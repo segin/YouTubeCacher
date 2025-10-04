1d36263349c17e42aaaa20eb97b4ebefbb4b76c0
Kirn Gill II <segin2005@gmail.com>
2025-10-04 14:18:22 -0500

    Implement resizable window with proper layout and button labels

--- main.c
+++ main.c
@@ -1,71 +1,95 @@
 #include <windows.h>
 
-// Window procedure function
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-        case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
+#define IDC_TEXT_FIELD    1001
+#define IDC_LABEL1        1002
+#define IDC_LABEL2        1003
+#define IDC_LABEL3        1004
+#define IDC_LIST          1005
+#define IDC_BUTTON1       1006
+#define IDC_BUTTON2       1007
+#define IDC_BUTTON3       1008
+
+#define IDD_MAIN_DIALOG   101
+
+void ResizeControls(HWND hDlg) {
+    RECT rect;
+    GetClientRect(hDlg, &rect);
+    
+    int width = rect.right - rect.left;
+    int height = rect.bottom - rect.top;
+    
+    // Resize URL text field
+    SetWindowPos(GetDlgItem(hDlg, IDC_TEXT_FIELD), NULL, 45, 8, width - 135, 14, SWP_NOZORDER);
+    
+    // Resize listbox (leave 90px for buttons + margin)
+    SetWindowPos(GetDlgItem(hDlg, IDC_LIST), NULL, 10, 70, width - 100, height - 80, SWP_NOZORDER);
+    
+    // Position buttons on the right
+    int buttonX = width - 80;
+    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON1), NULL, buttonX, 70, 70, 25, SWP_NOZORDER);
+    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON2), NULL, buttonX, 100, 70, 25, SWP_NOZORDER);
+    SetWindowPos(GetDlgItem(hDlg, IDC_BUTTON3), NULL, buttonX, 130, 70, 25, SWP_NOZORDER);
+}
+
+// Dialog procedure function
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_INITDIALOG:
+            // Initialize dialog controls
+            SetDlgItemText(hDlg, IDC_LABEL2, "Status: Ready");
+            SetDlgItemText(hDlg, IDC_LABEL3, "Items: 0");
             
-            // Fill background with light gray
-            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            // Add some sample items to the listbox
+            SendDlgItemMessage(hDlg, IDC_LIST, LB_ADDSTRING, 0, (LPARAM)"Sample Video 1.mp4");
+            SendDlgItemMessage(hDlg, IDC_LIST, LB_ADDSTRING, 0, (LPARAM)"Sample Video 2.mp4");
+            SendDlgItemMessage(hDlg, IDC_LIST, LB_ADDSTRING, 0, (LPARAM)"Sample Video 3.mp4");
             
-            // Draw some text
-            SetTextColor(hdc, RGB(0, 0, 0));
-            SetBkMode(hdc, TRANSPARENT);
-            TextOut(hdc, 20, 20, "Hello, Windows!", 15);
+            // Set minimum window size
+            SetWindowPos(hDlg, NULL, 0, 0, 400, 300, SWP_NOMOVE | SWP_NOZORDER);
             
-            EndPaint(hwnd, &ps);
+            return TRUE;
+            
+        case WM_SIZE:
+            ResizeControls(hDlg);
+            return TRUE;
+            
+        case WM_GETMINMAXINFO: {
+            MINMAXINFO* mmi = (MINMAXINFO*)lParam;
+            mmi->ptMinTrackSize.x = 350;
+            mmi->ptMinTrackSize.y = 250;
             return 0;
         }
+            
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case IDC_BUTTON1:
+                    MessageBox(hDlg, "Download functionality not implemented yet", "Download", MB_OK);
+                    break;
+                    
+                case IDC_BUTTON2:
+                    MessageBox(hDlg, "Play functionality not implemented yet", "Play", MB_OK);
+                    break;
+                    
+                case IDC_BUTTON3:
+                    MessageBox(hDlg, "Delete functionality not implemented yet", "Delete", MB_OK);
+                    break;
+                    
+                case IDCANCEL:
+                    EndDialog(hDlg, LOWORD(wParam));
+                    return TRUE;
+            }
+            break;
+            
+        case WM_CLOSE:
+            EndDialog(hDlg, 0);
+            return TRUE;
     }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    return FALSE;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    const char* CLASS_NAME = "BasicWindowClass";
-    
-    // Register window class
-    WNDCLASS wc = {0};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    
-    RegisterClass(&wc);
-    
-    // Create window
-    HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles
-        CLASS_NAME,                     // Window class
-        "Basic Windows Application",    // Window text
-        WS_OVERLAPPEDWINDOW,           // Window style
-        
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
-        
-        NULL,       // Parent window    
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        NULL        // Additional application data
-    );
-    
-    if (hwnd == NULL) {
-        return 0;
-    }
-    
-    ShowWindow(hwnd, nCmdShow);
-    
-    // Message loop
-    MSG msg = {0};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+    // Create and show the dialog
+    DialogBox(hInstance, MAKEINTRESOURCE(IDD_MAIN_DIALOG), NULL, DialogProc);
     
     return 0;
 }
